c6422cc1bba588fff51c5bed9032554142315b51
Erika Gili <erika.gili@icloud.com>
2021-03-25 11:27:27 +0100

    Changed title font

--- src/ThemeSetup.php
+++ src/ThemeSetup.php
@@ -91,7 +91,7 @@ class ThemeSetup {
 		?>
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css2?family=Texturina:wght@300;400;700&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
 		<?php
 	}
 
